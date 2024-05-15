9435bb359e614b5e0a976ecdc18719d37b454a1d
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-05-14 23:04:39 -0600

    colored printl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use colored::{self, Colorize};
 
 fn main() {
     println!("Guess the number!");
@@ -19,17 +20,20 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read line");
 
-        let guess: u32  = guess.trim().parse().expect("Please type a number");
+        let guess: u32  = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number){
-            Ordering::Less => println!("Too small!"),
+            Ordering::Less => println!("{}", "Too small!".red()),
             Ordering::Equal => {
-                println!("You win!");
+                println!("{}", "You win!".green());
                 break;
             },
-            Ordering::Greater => println!("Too big!")
+            Ordering::Greater => println!("{}", "Too big!".red())
         }
 
     }
